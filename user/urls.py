211fcb66e4d36e3d5c9e2f38c31eca9d57208481
Muzaffar <ibrohimovmuzaffar47@gmail.com>
2025-02-24 10:21:32 +0500

    add url send email

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('login-oauth-page/', views.login_page, name='login_page'),
     path('logout/', views.logout_page, name='logout_page'),
     path("register/", views.register, name="register"),
-    path('verify/', views.verify_email, name='verify'),
+    # path('verify/', views.verify_email, name='verify'),
+    path('verify-email/done/', views.verify_email_done, name='verify-email-done'),
+    path('verify-email-confirm/<uidb64>/<token>/', views.verify_email_confirm, name='verify-email-confirm'),
+    path('verify-email/complete/', views.verify_email_complete, name='verify-email-complete'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,4 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import RegisterForm
@@ -8,10 +6,16 @@ from django.core.mail import send_mail
 from user.forms import LoginForm
 from config.settings import DEFAULT_FROM_EMAIL
 from django.utils.crypto import get_random_string
-from django.contrib.auth import get_user_model
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from user.custom_token import account_activation_token
+from django.core.mail import EmailMessage
+from django.contrib import messages
+from django.contrib.auth.backends import ModelBackend
 
-User = get_user_model()  # Custom User modelini olish
-
+User = get_user_model()
 
 
 # Create your views here.
@@ -50,55 +54,86 @@ def register(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password1'])
             user.is_active = False
-            user.is_staff = True
-            user.is_superuser = True
             user.save()
+            current_site = get_current_site(request)
+            email = user.email
+            subject = "Verify Email"
+            message = render_to_string('user/email-verification/verify_email_message.html', {
+                'request': request,
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            # verification_code = get_random_string(6, '0123456789')  # 6 xonali kod
+            # request.session['verification_code'] = verification_code  # Sessiyada saqlash
+            # request.session['email'] = user.email
+            email_message = EmailMessage(subject, message, to=[email])
+            email_message.content_subtype = 'html'
+            email_message.send()
+            return render(request, 'user/email-verification/verify_email_done.html',{'email': user.email})
 
-            verification_code = get_random_string(6, '0123456789')  # 6 xonali kod
-            request.session['verification_code'] = verification_code  # Sessiyada saqlash
-            request.session['email'] = user.email
+        return render(request, 'user/register.html', {'form': form})
 
-            send_mail(
-                "Ro‘yxatdan o'tish tasdiqlash kodi",
-                f"Sizning tasdiqlash kodingiz: {verification_code}",
-                DEFAULT_FROM_EMAIL,
-                [user.email],
-                fail_silently=False
-            )
-            return redirect("user:verify")
-        else:
-            messages.error(request, "Registration failed. Please check your input.")
+    form = RegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+
+# def verify_email(request):
+#     if request.method == "POST":
+#         entered_code = request.POST.get("code")
+#         verification_code = request.session.get("verification_code")
+#         email = request.session.get("email")
+#
+#         if not verification_code or not email:
+#             messages.error(request, "Tasdiqlash kodi topilmadi. Iltimos, qayta ro‘yxatdan o‘ting.")
+#             return redirect("user:register")
+#
+#         if entered_code == verification_code:
+#             try:
+#                 user = User.objects.get(email=email)
+#                 user.is_active = True
+#                 user.save()
+#
+#                 del request.session["verification_code"]
+#                 del request.session["email"]
+#
+#                 login(request, user)
+#                 return redirect("ecommerce:index")
+#             except User.DoesNotExist:
+#                 messages.error(request, "Foydalanuvchi topilmadi!")
+#                 return redirect("user:register")
+#         else:
+#             messages.error(request, "Kod noto‘g‘ri!")
+#             return redirect("user:verify")
+#
+#     return render(request, "user/verify.html")
+
+
+def verify_email_confirm(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your email has been verified.')
+
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        login(request, user)
+        return redirect('user:verify-email-complete')
     else:
-        form = RegisterForm()
+        messages.warning(request, 'The link is invalid.')
+    return render(request, 'user/email-verification/verify_email_confirm.html')
 
-    return render(request, "user/register.html", {"form": form})
 
-def verify_email(request):
-    if request.method == "POST":
-        entered_code = request.POST.get("code")
-        verification_code = request.session.get("verification_code")
-        email = request.session.get("email")
+def verify_email_complete(request):
+    return render(request, 'user/email-verification/verify_email_complete.html')
 
-        if not verification_code or not email:
-            messages.error(request, "Tasdiqlash kodi topilmadi. Iltimos, qayta ro‘yxatdan o‘ting.")
-            return redirect("user:register")
-
-        if entered_code == verification_code:
-            try:
-                user = User.objects.get(email=email)
-                user.is_active = True
-                user.save()
-
-                del request.session["verification_code"]
-                del request.session["email"]
-
-                login(request, user)
-                return redirect("ecommerce:index")
-            except User.DoesNotExist:
-                messages.error(request, "Foydalanuvchi topilmadi!")
-                return redirect("user:register")
-        else:
-            messages.error(request, "Kod noto‘g‘ri!")
-            return redirect("user:verify")
-
-    return render(request, "user/verify.html")
+def verify_email_done(request):
+    return render(request, 'user/email-verification/verify_email_done.html')
